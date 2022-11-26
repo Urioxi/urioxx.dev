e713248ffca49b9b455f2a4a0332e3c28a15bd20
Urioxi <urioxa@gmail.com>
2022-11-26 16:59:24 +0100

    test fail navopoen #1

--- js/script.js
+++ js/script.js
@@ -13,28 +13,3 @@ listItems.forEach(item => {
     item.classList.add('active')
 }
 })
-
-
-Vue.config.productionTip = false
-
-Vue.component('start', {
-  template: '#start'
-})
-
-Vue.component('next', {
-  template: '#next'
-})
-
-new Vue({
-  el: '#app',
-  
-  data: {
-    page: 'start'
-  },
-  
-  methods: {
-    onChangePage(to) {
-      this.page = to
-    }
-  }
-})
