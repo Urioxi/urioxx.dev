a12ad09ab63c55712fddcd2e098586f86eefb8c8
Urioxi <urioxa@gmail.com>
2022-11-26 16:46:03 +0100

    patch bug1 bdt

--- js/script.js
+++ js/script.js
@@ -15,5 +15,26 @@ listItems.forEach(item => {
 })
 
 
-//Swup
-const swup = new Swup();
+Vue.config.productionTip = false
+
+Vue.component('start', {
+  template: '#start'
+})
+
+Vue.component('next', {
+  template: '#next'
+})
+
+new Vue({
+  el: '#app',
+  
+  data: {
+    page: 'start'
+  },
+  
+  methods: {
+    onChangePage(to) {
+      this.page = to
+    }
+  }
+})
